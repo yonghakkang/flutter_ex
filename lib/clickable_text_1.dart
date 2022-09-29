25b74d05faf1f9c378b7a82f43ca09172614b2fe
yonghakkang <zihover@gmail.com>
2022-09-30 01:47:51 +0900

    ADD - clickable text 기능추가&폰트적용-   확인하시기전에 먼저 만들보시길 권장드려요~!

--- lib/clickable_text_1.dart
+++ lib/clickable_text_1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fade_out_particle/fade_out_particle.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +10,33 @@ class MyApp extends StatefulWidget {
 
 }
 
+
 class _MyAppState extends State<MyApp> {
+  bool firstVisit = true;
   bool isFlip = false;
-  var whatYouWant = "hello World.";
+  bool isDisappear = false;
+  var whatYouWant = "Hello  World";
+
+  _setValues(withOutFlip){
+
+    isFlip = (withOutFlip)? (!isFlip):isFlip;
+
+    if(!firstVisit){
+
+      whatYouWant = (isFlip)?"Our dreams\n "
+          "will come true\n"
+          "\n"
+          "Let's believe in\n "
+          "our possibilities"
+          :
+      "I believe this will be a really cool app.\n "
+          "Because we'll make it happen!!\n"
+          "        . . .\n"
+          "Let's look in the same direction\n "
+          "& let's make success!!";
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,18 +50,36 @@ class _MyAppState extends State<MyApp> {
           child: GestureDetector(
             onTap: () {
               setState(() {
+                _setValues(false);
+              });
+              Future.delayed(const Duration(milliseconds: 1000), () {
 
-                isFlip = !isFlip;
-
-                whatYouWant = (isFlip)?"Our dreams will come true. ^^.":
-                "I believe this will be a really cool app.\n Because we'll make it happen!!";
-
+                setState(() {
+                  isDisappear = true;
+                });
 
               });
 
-              print("Let's make ours Dreams come true");
+              print("Let's make ours Dreams come true.");
             },
-            child:Text(whatYouWant),
+            child: FadeOutParticle(
+              duration: Duration(milliseconds: 2000),
+              disappear: isDisappear,
+              child:  Text(whatYouWant,style: Theme.of(context).textTheme.headline6?.copyWith(
+                  //fontWeight: (!isFlip)?FontWeight.w900:FontWeight.w100,
+                  fontSize: (!isFlip)?48:30,
+                  fontFamily:(firstVisit)?'hugs':(!isFlip)?'helloketta':'whoa'
+              ),
+              ),
+              onAnimationEnd: (){
+                setState(() {
+                  firstVisit = false;
+                  isDisappear = false;
+                  _setValues(true);
+                });
+              },
+            ),
+
             // child: (isFlip)?
             //   Text("Our dreams will come true."):
             //   Text("Hello world"),
